b0aedd52560437138d03d2174ba38d8fa49ede81
Olexandr-L <sasha.lishak@gmail.com>
2026-08-18 12:01:17 +0300

    feat: add new category and questions for rights and obligations; include related images

--- src/app/core/maps/categories.map.ts
+++ src/app/core/maps/categories.map.ts
@@ -3,5 +3,10 @@ export const categories = [
         id: 1,
         path: 'assets/questions/generalProvisions.json',
         name: 'Загальні положення',
+    },
+    {
+        id: 2,
+        path: 'assets/questions/rightsAndObligations.json',
+        name: 'Обов\'язки і права водіїв механічних транспортних засобів',
     }
 ]
